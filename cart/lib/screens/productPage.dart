be81d00b78b24179d6fc330a1c2a5287970e51e5
Akil <akilvishnum@gmail.com>
2021-03-15 23:58:26 +0530

    Changed color to HSL | Gradient using HSL with different Lightness Values

--- cart/lib/screens/productPage.dart
+++ cart/lib/screens/productPage.dart
@@ -8,6 +8,7 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   String productImage = 'assets/products/phone.png';
   PaletteColor productBackground;
+  HSLColor light, dark;
   @override
   
   void initState(){
@@ -23,11 +24,19 @@ class _ProductPageState extends State<ProductPage> {
        AssetImage(productImage), size: Size(100000, 100000),
      );
      productBackground = generator.dominantColor != null ? generator.dominantColor : PaletteColor(Colors.black, 2);
+     HSLColor productHSL = HSLColor.fromColor(productBackground.color);
+     light = productHSL.withLightness(0.7);
+     dark = productHSL.withLightness(0.3);
      setState(() {});
   }
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:  productBackground != null ? productBackground.color : Color.fromRGBO(229, 229, 229, 1),
+    return Container(
+      //backgroundColor:  productBackground != null ? productBackground.color : Color.fromRGBO(229, 229, 229, 1),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [light != null ? light.toColor() : Color.fromRGBO(229, 229, 229, 1), dark != null ? dark.toColor() : Color.fromRGBO(229, 229, 229, 1)]),
+      ),
     );
   }
 }
