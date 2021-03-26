d7d0d2b297d2bdd20b5fc155b5e57c572c5edf1c
Akil <akilvishnum@gmail.com>
2021-03-26 12:27:33 +0530

    Added Action Sheet Skin

--- cart/lib/main.dart
+++ cart/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        canvasColor: Colors.transparent,
       ),
       home: WelcomePage(),
     );
