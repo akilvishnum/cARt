09e0a72a720e9ca3e75444aeb73744a55930227e
Akil <akilvishnum@gmail.com>
2021-04-10 01:20:18 +0530

    Added Product list display in Cartpage.

--- cart/lib/screens/CartPage.dart
+++ cart/lib/screens/CartPage.dart
@@ -1,66 +1,78 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'package:flutter_svg/flutter_svg.dart';
-class CartPage extends StatefulWidget{
+
+class CartPage extends StatefulWidget {
   @override
   _CartPageState createState() => _CartPageState();
 }
-class _CartPageState extends State<CartPage>{
-  Widget build(BuildContext context){
+
+class _CartPageState extends State<CartPage> {
+  Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Container(
-        width: width, height: height,
+        width: width,
+        height: height,
         color: Colors.black,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:[
+          children: [
             Container(
-              height: height * 0.12,
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children:[
-                  Expanded(
-                    child: Stack(
-                      children:[
-                        Center(
-                          child: Container(
-                            child: Text(
-                              'Cart',
-                              style: TextStyle(
-                                  fontSize: height * 0.03,
-                                  fontFamily: 'Bold',
-                                  color: Colors.white, letterSpacing: height * 0.03 * 0.01)
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          left: 0, top: 0, bottom: 0,
-                          child: Center(
+                height: height * 0.12,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Stack(
+                        children: [
+                          Center(
                             child: Container(
-                              height: height * 0.065,
-                              width: height * 0.065,
-                              decoration: BoxDecoration(
-                                color: Color.fromRGBO(90, 90, 90, 100),
-                                borderRadius: BorderRadius.circular(height * 0.022),
-                              ),
-                              child: Center(child:SvgPicture.asset(
-                                'assets/icons/back.svg',
-                                color: Colors.white,
-                              ), ),
+                              child: Text('Cart',
+                                  style: TextStyle(
+                                      fontSize: height * 0.03,
+                                      fontFamily: 'Bold',
+                                      color: Colors.white,
+                                      letterSpacing: height * 0.03 * 0.01)),
                             ),
                           ),
-                        ),
-                      ],
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            bottom: 0,
+                            child: InkWell(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child:Center(
+                              child: Container(
+                                height: height * 0.065,
+                                width: height * 0.065,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(90, 90, 90, 100),
+                                  borderRadius:
+                                      BorderRadius.circular(height * 0.022),
+                                ),
+                                child: Center(
+                                  child: SvgPicture.asset(
+                                    'assets/icons/back.svg',
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ),
+                  ],
+                )),
             Expanded(
               child: Container(
+                padding: EdgeInsets.only(left: 10, right: 10),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -68,21 +80,23 @@ class _CartPageState extends State<CartPage>{
                       topLeft: Radius.circular(height * 0.05)),
                 ),
                 child: Stack(
-                  children:[
+                  children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children:[
-                        Expanded(
-                        child: ConstrainedBox(
+                      children: [
+                        SizedBox(height: height * 0.025),
+                        ConstrainedBox(
                           constraints: BoxConstraints(maxHeight: 5000),
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          physics: BouncingScrollPhysics(),
-                          itemCount: 3,
-                          itemBuilder: (BuildContext context, int index) {
-                              return CartProductContainer();
+                          child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: BouncingScrollPhysics(),
+                              itemCount: 3,
+                              itemBuilder: (BuildContext context, int index) {
+                                return CartProductContainer();
                           }),
-                        ), ),
+                        ),
+                        SizedBox(height: height * 0.0125),
+                        Text('SUMMARY'),
                       ],
                     ),
                     Positioned(
@@ -161,8 +175,7 @@ class _CartPageState extends State<CartPage>{
                                                             FontWeight.bold,
                                                         fontSize: 18,
                                                         fontFamily: 'Bold',
-                                                        color:
-                                                            Colors.white)),
+                                                        color: Colors.white)),
                                               ],
                                             ),
                                           ),
@@ -187,13 +200,74 @@ class _CartPageState extends State<CartPage>{
     );
   }
 }
+
 class CartProductContainer extends StatelessWidget {
-  Widget build(BuildContext context){
-    return Container(
-      child: Text('Hello'),
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 10, right: 10, top: 12, bottom: 10),
+          // height: width * 0.2
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(248, 248, 248, 1),
+            borderRadius: BorderRadius.all(Radius.circular(width * 0.05)),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: new Image.asset(
+                  'assets/products/phone.png',
+                  width: width * 0.2,
+                  height: width * 0.2,
+                ),
+              ),
+              SizedBox(width: 20),
+              Container(
+                height: width * 0.2,
+                width: width * 0.4,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Soft Sofa - Peachy Pink',
+                      style: TextStyle(
+                        fontFamily: 'Bold',
+                        color: Colors.black,
+                        fontSize: 20,
+                      ),
+                      overflow: TextOverflow.clip,
+                      maxLines: 2,
+                    ),
+                    Text(
+                      '\$1123',
+                      style: TextStyle(
+                        fontFamily: 'Medium',
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                      overflow: TextOverflow.clip,
+                      maxLines: 1,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: ProductCounter(),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 15),
+      ],
     );
   }
 }
+
 Widget buildBlur(
         {@required Widget child, double sigmaX = 10, double sigmaY = 10}) =>
     ClipRRect(
@@ -202,3 +276,80 @@ Widget buildBlur(
         child: child,
       ),
     );
+
+class ProductCounter extends StatefulWidget {
+  @override
+  _ProductCounterState createState() => _ProductCounterState();
+}
+
+class _ProductCounterState extends State<ProductCounter> {
+  var count = 1;
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Container(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() {
+                if (count > 1) {
+                  count--;
+                } else
+                  count = 1;
+              });
+            },
+            child: Container(
+              width: width * 0.065,
+              height: width * 0.065,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color:
+                      count == 1 ? Colors.red.withOpacity(0.3) : Colors.black),
+              child: Center(
+                child: SvgPicture.asset(
+                  count == 1 ? 'assets/icons/AR.svg' : 'assets/icons/back.svg',
+                  color: count == 1 ? Colors.red : Colors.white,
+                  width: width * 0.035,
+                  height: width * 0.035,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: width * 0.008),
+          Container(
+              child: Text(count.toString(),
+                  style:
+                      TextStyle(fontSize: width * 0.06, fontFamily: 'Medium'))),
+          SizedBox(width: width * 0.008),
+          InkWell(
+            onTap: () {
+              setState(() {
+                count++;
+              });
+            },
+            child: Container(
+              width: width * 0.065,
+              height: width * 0.065,
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: Colors.black),
+              child: Center(
+                child: SvgPicture.asset(
+                  'assets/icons/back.svg',
+                  color: Colors.white,
+                  width: width * 0.035,
+                  height: width * 0.035,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
